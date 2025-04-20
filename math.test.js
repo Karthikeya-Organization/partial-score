6e479cab62589132a476e01e07ceb5d41e9496e3
karthikeya890 <bunnymkk9@gmail.com>
2025-04-20 16:32:37 +0530

    second commit

---
+++ math.test.js
@@ -0,0 +1,20 @@
+test('adds 1 + 1 to equal 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+  
+  test('adds 2 + 2 to equal 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+  
+  test('adds 2 + 3 to equal 5', () => {
+    expect(2 + 3).toBe(5);
+  });
+  
+  test('adds 5 + 5 to equal 10', () => {
+    expect(5 + 5).toBe(10);
+  });
+  
+  test('adds 10 + 1 to equal 11', () => {
+    expect(10 + 1).toBe(11);
+  });
+  
